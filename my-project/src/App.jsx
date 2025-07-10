01832378e7699b7098c85a4bea74ec9ba1b00a66
RadhikaSuryawanshi <radhikasuryawanshi279@gmail.com>
2025-07-10 21:36:17 +0530

    Responsive Design

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-full h-screen flex flex-col '>
-      <div class="navbar bg-base-100 ">
-         
+    <div className='w-full h-screen  flex flex-col '>
+      <div class="navbar flex justify-center  lg:justify-start">
+
          <div class="relative inline-block text-left">
-         <div class="dropdown ">
+         <div class="dropdown dropdown-start lg:dropdown">
   <div tabindex="0" role="button" class="btn m-1 text-xl">menu <svg class="-mr-1 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
         <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
       </svg></div>
@@ -122,22 +122,22 @@ const App = () => {
 </div>
          </div>
       </div>
-      <div className='w-full h-1/2  flex justify-center items-center mt-15'>
-    <div className='w-[80%] h-full flex flex-row justify-center text-center items-center'>
+      <div className='w-full lg:h-1/2  flex flex-col lg-flex-row justify-center items-center md:mt-5 lg:mt-15'>
+    <div className='w-[90%] h-full flex lg:flex-row  flex-col justify-center text-center items-center lg:p-10'>
     <div className='w-1/3 h-full flex flex-col justify-center item-center gap-4'>
-<img src="/profile.png" alt="profile" className='size-70 w-full f-full object-cover object-top   hover:scale-105 transition-all duration-300 glow-image' />
-<p className='text-xl underline underline-offset-2'><i>Radhika Sanjay Suryawanshi</i></p>
+<img src="/profile.png" alt="profile" className='size-64 md:size-80 lg:w-full lg:h-full object-cover object-top   lg:hover:scale-105 transition-all duration-300 glow-image' />
+<p className=' text-sm  underline underline-offset-2 lg:text-xl inline '><i className='font-light '>Radhika Sanjay Suryawanshi</i></p>
     </div>
       <div className='w-2/3 h-full  flex justify-center items-center mx-5'>
-<p className='w-full font-semibold text-5xl leading-20 '>Passionate developer dedicated to turning ideas&nbsp;<span class="animate-pulse glow">💡</span>
+<p className='w-full font-semibold text-2xl lg:text-5xl leading-10 lg:leading-20 '>Passionate developer dedicated to turning ideas&nbsp;<span class="animate-pulse glow">💡</span>
 &nbsp;into stunning digital experiences. </p>
     </div>
     
     </div>
     </div>
-    <div className='w-full  flex flex-col justify-center items-center mt-15 '>
+    <div className='w-full  flex flex-col justify-center items-center mt-5 lg:mt-15 '>
       <p className='text-xl hover:scale-105 duration-150'>"Let's get started — big things ahead!"</p> 
-      <div className='  flex gap-2 justify-center items-center mt-5 '>
+      <div className='  flex gap-2 justify-center items-center mt-2 lg:mt-5 '>
       <div class="badge badge-primary badge-xs"></div>
       <div class="badge badge-primary badge-sm"></div>
       <div class="badge badge-primary badge-md"></div>
